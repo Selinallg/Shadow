edc601b722432ef5c175d11f72f865e798aeb167
msi-liu.lg@163.com <liu.lg@163.com>
2024-12-13 20:03:03 +0800

    关键代码移动成功

---
+++ shadow/coding/common-jar-settings/build/generateCode/com/tencent/shadow/coding/common_jar_settings/AndroidJar.java
@@ -0,0 +1,7 @@
+package com.tencent.shadow.coding.common_jar_settings;
+
+import java.lang.String;
+
+public final class AndroidJar {
+  public static final String ANDROID_JAR_PATH = "D:\\Android\\sdk\\platforms\\android-33\\android.jar";
+}
---
+++ shadow/coding/java-build-config/build/generateCode/com/tencent/shadow/coding/java_build_config/BuildConfig.java
@@ -0,0 +1,7 @@
+package com.tencent.shadow.coding.java_build_config;
+
+import java.lang.String;
+
+public final class BuildConfig {
+  public static final String VERSION_NAME = "local";
+}
