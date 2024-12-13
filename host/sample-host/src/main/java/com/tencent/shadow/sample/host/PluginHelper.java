72d12d3f15c98cc0a8bed53552872c964c45b003
msi-liu.lg@163.com <liu.lg@163.com>
2024-12-13 20:15:45 +0800

    平移sample 到 host

---
+++ host/sample-host/src/main/java/com/tencent/shadow/sample/host/PluginHelper.java
@@ -0,0 +1,103 @@
+/*
+ * Tencent is pleased to support the open source community by making Tencent Shadow available.
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company.  All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *     https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.tencent.shadow.sample.host;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.tencent.shadow.core.common.Logger;
+import com.tencent.shadow.core.common.LoggerFactory;
+import com.tencent.shadow.dynamic.host.DynamicPluginManager;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class PluginHelper {
+
+    private static final String TAG = "PluginHelper";
+
+    /**
+     * 动态加载的插件管理apk
+     */
+    public final static String sPluginManagerName = "pluginmanager.apk";
+
+    /**
+     * 动态加载的插件包，里面包含以下几个部分，插件apk，插件框架apk（loader apk和runtime apk）, apk信息配置关系json文件
+     */
+    public final static String sPluginZip = BuildConfig.DEBUG ? "plugin-debug.zip" : "plugin-release.zip";
+
+    public File pluginManagerFile;
+
+    public File pluginZipFile;
+
+    public ExecutorService singlePool = Executors.newSingleThreadExecutor();
+
+    private Context mContext;
+
+    private static PluginHelper sInstance = new PluginHelper();
+
+    public static PluginHelper getInstance() {
+        return sInstance;
+    }
+
+    private PluginHelper() {
+    }
+
+    public void init(Context context) {
+        pluginManagerFile = new File(context.getFilesDir(), sPluginManagerName);
+        pluginZipFile = new File(context.getFilesDir(), sPluginZip);
+
+        mContext = context.getApplicationContext();
+
+        singlePool.execute(new Runnable() {
+            @Override
+            public void run() {
+                preparePlugin();
+            }
+        });
+
+    }
+
+    // pluginManagerFile=/data/user/0/com.tencent.shadow.sample.host/files/pluginmanager.apk
+    // pluginZipFile=/data/user/0/com.tencent.shadow.sample.host/files/plugin-debug.zip
+    private void preparePlugin() {
+        try {
+            InputStream is = mContext.getAssets().open(sPluginManagerName);
+            FileUtils.copyInputStreamToFile(is, pluginManagerFile);
+            Log.d(TAG, "preparePlugin: pluginManagerFile="+pluginManagerFile.getAbsolutePath());
+//            Logger mLogger = LoggerFactory.getLogger(DynamicPluginManager.class);
+//            AndroidLogLoggerFactory.getInstance().getLogger("xx").debug("xx");
+            // TODO: 2024-12-13 后期可以从网络上下载动态加载的插件包，然后解压到本地，然后再加载
+            InputStream zip = mContext.getAssets().open(sPluginZip);
+            FileUtils.copyInputStreamToFile(zip, pluginZipFile);
+
+            Log.d(TAG, "preparePlugin: pluginZipFile="+pluginZipFile.getAbsolutePath());
+
+        } catch (IOException e) {
+            throw new RuntimeException("从assets中复制apk出错", e);
+        }
+    }
+
+
+}
