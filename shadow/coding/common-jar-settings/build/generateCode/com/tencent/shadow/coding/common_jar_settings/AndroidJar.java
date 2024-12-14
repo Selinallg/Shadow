89b0ccd6198d09ec6833fb9f427f0c82f3f62896
msi-liu.lg@163.com <liu.lg@163.com>
2024-12-14 15:07:54 +0800

    干掉一部分 build 临时文件

--- shadow/coding/common-jar-settings/build/generateCode/com/tencent/shadow/coding/common_jar_settings/AndroidJar.java
+++
@@ -1,7 +0,0 @@
-package com.tencent.shadow.coding.common_jar_settings;
-
-import java.lang.String;
-
-public final class AndroidJar {
-  public static final String ANDROID_JAR_PATH = "D:\\Android\\sdk\\platforms\\android-33\\android.jar";
-}
--- shadow/coding/java-build-config/build/generateCode/com/tencent/shadow/coding/java_build_config/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-package com.tencent.shadow.coding.java_build_config;
-
-import java.lang.String;
-
-public final class BuildConfig {
-  public static final String VERSION_NAME = "local";
-}
