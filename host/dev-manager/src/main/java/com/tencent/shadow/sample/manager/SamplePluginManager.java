3d0dc08275253a8974e7fb7449a124f36450830c
msi-liu.lg@163.com <liu.lg@163.com>
2024-12-17 11:03:05 +0800

    增加 plugin service 测试模块，未进行验证

--- host/dev-manager/src/main/java/com/tencent/shadow/sample/manager/SamplePluginManager.java
+++ host/dev-manager/src/main/java/com/tencent/shadow/sample/manager/SamplePluginManager.java
@@ -22,9 +22,12 @@ import static com.nolovr.shadow.core.constant.Constant.PART_KEY_PLUGIN_ANOTHER_A
 import static com.nolovr.shadow.core.constant.Constant.PART_KEY_PLUGIN_BASE;
 import static com.nolovr.shadow.core.constant.Constant.PART_KEY_PLUGIN_MAIN_APP;
 
+import android.app.Service;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -33,6 +36,8 @@ import android.view.View;
 import com.tencent.shadow.core.manager.installplugin.InstalledPlugin;
 import com.tencent.shadow.dynamic.host.EnterCallback;
 import com.nolovr.shadow.core.constant.Constant;
+import com.tencent.shadow.dynamic.loader.PluginServiceConnection;
+import com.tencent.shadow.sample.plugin.IMyAidlInterface;
 
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -173,6 +178,60 @@ public class SamplePluginManager extends FastPluginManager {
                 }
                 if (callback != null) {
                     callback.onCloseLoadingView();
+                }
+            }
+        });
+    }
+
+
+
+    private void callPluginService(final Context context, Bundle bundle, final EnterCallback callback) {
+//        final String pluginZipPath = "/data/local/tmp/plugin-debug.zip";
+//        final String partKey = "sample-plugin";
+//        final String className = "com.tencent.shadow.sample.plugin.MyService";
+
+        final String pluginZipPath = bundle.getString(Constant.KEY_PLUGIN_ZIP_PATH);
+        final String partKey = bundle.getString(Constant.KEY_PLUGIN_PART_KEY);
+        final String className = bundle.getString(Constant.KEY_ACTIVITY_CLASSNAME);
+
+        Intent pluginIntent = new Intent();
+        pluginIntent.setClassName(context.getPackageName(), className);
+
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    InstalledPlugin installedPlugin
+                            = installPlugin(pluginZipPath, null, true);//这个调用是阻塞的
+
+                    loadPlugin(installedPlugin.UUID, partKey);
+
+                    Intent pluginIntent = new Intent();
+                    pluginIntent.setClassName(context.getPackageName(), className);
+
+                    boolean callSuccess = mPluginLoader.bindPluginService(pluginIntent, new PluginServiceConnection() {
+                        @Override
+                        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                            IMyAidlInterface iMyAidlInterface = IMyAidlInterface.Stub.asInterface(iBinder);
+                            try {
+                                String s = iMyAidlInterface.basicTypes(1, 2, true, 4.0f, 5.0, "6");
+                                Log.i("SamplePluginManager", "iMyAidlInterface.basicTypes : " + s);
+                            } catch (RemoteException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+
+                        @Override
+                        public void onServiceDisconnected(ComponentName componentName) {
+                            throw new RuntimeException("onServiceDisconnected");
+                        }
+                    }, Service.BIND_AUTO_CREATE);
+
+                    if (!callSuccess) {
+                        throw new RuntimeException("bind service失败 className==" + className);
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
                 }
             }
         });
---
+++ plugins/zip/plugin-service-lib/src/main/java/com/tencent/shadow/sample/plugin/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tencent.shadow.sample.plugin;
+
+import android.os.Bundle;
+
+import com.tencent.shadow.sample.common.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
